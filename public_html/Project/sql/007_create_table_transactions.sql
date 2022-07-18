52f63224c2b18fafbe036da868cc164f04d922dd
kp <kp55@njit.edu>
2022-07-17 22:26:41 -0400

    Created Transactions Table

---
+++ public_html/Project/sql/007_create_table_transactions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `Transactions` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `account_src` INT NOT NULL, 
+    `account_dest` INT NOT NULL,
+    `balance_change` INT NOT NULL DEFAULT 0, 
+    `transaction_type` VARCHAR(20) NOT NULL,
+    `memo` VARCHAR(100) NOT NULL,
+    `expected_total` INT NOT NULL DEFAULT 0,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(account_src) REFERENCES Accounts(id),
+    FOREIGN KEY(account_dest) REFERENCES Accounts(id)
+)
