7af477762b2ee6ca99c780bbac70eacec6b91d46
kp <kp55@njit.edu>
2022-07-25 18:23:01 -0400

    Internal Transfer functionality

--- public_html/Project/dashboard.php
+++ public_html/Project/dashboard.php
@@ -4,13 +4,12 @@ is_logged_in(true);
 ?>
 <h1>Dashboard</h1>
     <ul class="centerDashboard">
-        <!--<li><a href="transfer.php">Transfer</a></li>-->
         <li><a href="profile.php">Profile</a></li>
         <li><a href="create_account.php">Create Account</a></li>
         <li><a href="my_accounts.php">My Accounts</a></li> 
         <li><a href="deposit_withdraw.php?type=Deposit">Deposit</a></li>
         <li><a href="deposit_withdraw.php?type=Withdraw">Withdraw</a></li>
-        <li><a href="#">Transfer</a></li>
+        <li><a href="transfer.php">Transfer</a></li>
     </ul>
 <style>
     ul.centerDashboard {
