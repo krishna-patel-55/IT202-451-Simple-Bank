1f0ad1cc80e04f4344c4cf0103734a308637b6aa
kp <kp55@njit.edu>
2022-09-09 17:40:24 -0400

    intro

---
+++ public_html/Project/intro.php
@@ -0,0 +1,17 @@
+<?php
+require(__DIR__ . "/../../partials/nav.php");
+?>
+<h1>Simple Bank</h1>
+<h3 style="text-align:center;margin-top: 50px;">About</h3>
+<p style="margin-left:20%;margin-right:20%;text-align:center;">This is a project for a web development class at NJIT, course IT 202.
+    This project is a bank simulation for users. 
+    Users will be able to have various accounts, 
+    do standard bank functions like deposit, withdraw, 
+    internal/external transfers, and creating/closing accounts.</p>
+<h6 style="text-align:center"><a href="https://youtu.be/062qMTVLw4c">Video Demo </a></h6>
+<h6 style="text-align:center">Developer: Krishna Patel</h6>
+<h6 style="text-align:center">Developed: May 2022</h6>
+</div>
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
